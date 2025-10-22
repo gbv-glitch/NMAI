339247a9b456d713a56d602e4c173a91792cf8b4
gbv-glitch <gustav.vendelin@gmail.com>
2025-10-22 12:15:29 +0300

    Played around with control surfaces, added clouds, ability to pause, control surfaces need to be finished

--- Assets/Scripts/PlaneControls.cs
+++ Assets/Scripts/PlaneControls.cs
@@ -4,11 +4,18 @@ using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UnityConsent;
 
-public class PlenFly : MonoBehaviour
+public class PlaneControls : MonoBehaviour
 {
     //Kaamera "giidi" positioon
     public Transform mainCameraGuidePosition;
+
+    //Lennuki osade positioon
+    public Transform leftCanard;
+    public Transform rightCanard;
+    public Transform rightElevon;
+    public Transform leftElevon;
 
     //Lennuki positioon
     public Transform playerPosition;
@@ -23,40 +30,64 @@ public class PlenFly : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
 
+    //Pausile panemise võimalus
+    public bool pause = false;
+
     //Mängu sündmused toimuvad siin
     private void Update()
     {
-        //Liigutame  lennuki edasi
-        transform.position += transform.forward * planeSpeed * Time.deltaTime;//Viimane on seal, et kõigil oleks ükskõik mis arvutil sama kiirus
+        //Siin me vaatame, kas mäng on pausile pandud
+        if (pause == true)
+        {    //Liigutame  lennuki edasi
+            transform.position += transform.forward * planeSpeed * Time.deltaTime;//Viimane on seal, et kõigil oleks ükskõik mis arvutil sama kiirus
 
-        //Me siin registreerime, mida mängija tahab teha
-        horizontalInput = Input.GetAxis("Horizontal") * 3;
-        verticalInput = -(Input.GetAxis("Vertical") * 5);
+            //Me siin registreerime, mida mängija tahab teha
+            horizontalInput = Input.GetAxis("Horizontal") * 4;
+            verticalInput = -(Input.GetAxis("Vertical") * 5);
 
-        //Siin paneme info oma muutujasse
-        yaw += horizontalInput * Time.deltaTime * 25;
-        pitch = Mathf.Lerp(0, 20, Mathf.Abs(verticalInput)) * MathF.Sign(verticalInput * Time.deltaTime);
-        roll = Mathf.Lerp(0, 20, Mathf.Abs(horizontalInput)) * -MathF.Sign(horizontalInput * Time.deltaTime);
+            //Siin paneme info oma muutujasse
+            yaw += horizontalInput * Time.deltaTime * 25;
+            pitch = Mathf.Lerp(0, 20, Mathf.Abs(verticalInput)) * MathF.Sign(verticalInput * Time.deltaTime);
+            roll = Mathf.Lerp(0, 30, Mathf.Abs(horizontalInput)) * -MathF.Sign(horizontalInput * Time.deltaTime);
 
-        //Siin me pöörame oma mängijat
-        transform.rotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll * 3);
+            //Pöörame lennuki osad, et see näeb realistilisem välja
+            if (roll != 0)
+            {
+                //rightElevon. = Quaternion.Euler(new Vector3(45, 0, 0));
+                //leftElevon.lo = Quaternion.Euler(new Vector3(45, 0, 0));
+            }
 
-        //Siin me toome kaamera mängija järel
-        mainCameraGuidePosition.position = playerPosition.position;
+            //Siin me pöörame oma mängijat
+            transform.rotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll * 3);
 
-        //Siin me pöörame oma kaamerat mängija järel
-        mainCameraGuidePosition.Rotate(0, horizontalInput * Time.deltaTime * 25, 0);
+            //Siin me toome kaamera mängija järel
+            mainCameraGuidePosition.position = playerPosition.position;
 
-        //Siin me muudame mängija kiirust
-        planeSpeed += Input.mouseScrollDelta.y;
+            //Siin me pöörame oma kaamerat mängija järel
+            mainCameraGuidePosition.Rotate(0, horizontalInput * Time.deltaTime * 25, 0);
 
-        //Selleks et mängija ei saaks kohal seista, kontrollime, kas lennukiirus on väiksem kui 15. Kui on, siis me paneme selle 15ks.
-        if(planeSpeed <= 15)
-        {
-            planeSpeed = 15;
+            //Siin me muudame mängija kiirust
+            planeSpeed += Input.mouseScrollDelta.y;
+
+            //Selleks et mängija ei saaks kohal seista, kontrollime, kas lennukiirus on väiksem kui 15. Kui on, siis me paneme selle 15ks.
+            if (planeSpeed <= 15)
+            {
+                planeSpeed = 15;
+            }
         }
 
-        print((Input.mouseScrollDelta.y, planeSpeed));
+        //Siin me saame panna mängu pausile või selle jälle käima panna
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (pause == true)
+            {
+                pause = false;
+            }
+            else
+            {
+                pause = true;
+            }
+        }
     }
     
 }
